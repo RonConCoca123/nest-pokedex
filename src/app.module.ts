6589e4aa41ab078d6c2158dc1cce717bdc765198
Daniel Zavaleta Nuñez <114974428+RonConCoca123@users.noreply.github.com>
2024-06-10 13:16:57 -0500

    Seed and Pagination
    Creamos la semilla para base de datos, y su paginación para busqueda con limite y offset

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { SeedModule } from './seed/seed.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
     CommonModule,
-    SeedModule
+    SeedModule,
   ],
 
 })
